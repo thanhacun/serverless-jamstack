08513f83c2d815649999210581f2aa772e7ba954
Thanh Truong <thanhacun@gmail.com>
2022-12-16 15:29:00 +0700

    first revise

---
+++ packages/algolia/getTho/index.js
@@ -0,0 +1,38 @@
+const algoliasearch = require("algoliasearch");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
+const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
+const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
+
+const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
+const index = client.initIndex(ALGOLIA_INDEX_NAME);
+
+function searchForText(index=index, searchText) {
+    return new Promise(resolve => {
+        index.search(searchText).then((objects) => {
+            resolve({
+                "body": objects.hits,
+                "statusCode": 200
+            });
+        })
+    })
+}
+
+async function main(args){
+    let searchText = args.search;
+    try {
+        const result = await searchForText(index, searchText);
+        return result;
+    } catch (e) {
+        console.error(e);
+        return {
+            "body": {"error": "There was a problem searching for "},
+            "statusCode": 400
+        };
+    }
+}
+
+module.exports.main = main;
--- web/script.js
+++ web/script.js
@@ -1,89 +1,99 @@
 //start of get request to retrieve available coffees
 
-const getInventory = async () => {
-  let results = await axios.get('/api/cloud/getCoffee');
-results.data.forEach(item => {
-  let pic = item.pic;
-  let name = item.name;
-  let description = item.description;
-  let price = item.price;
+// const getInventory = async () => {
+//   let results = await axios.get('/api/cloud/getCoffee');
+// results.data.forEach(item => {
+//   let pic = item.pic;
+//   let name = item.name;
+//   let description = item.description;
+//   let price = item.price;
 
-  let table = document.getElementById("product-table");
-  let newRow = table.insertRow(-1);
-  let picCell = newRow.insertCell();
-  let nameCell = newRow.insertCell();
-  let priceCell = newRow.insertCell();
+//   let table = document.getElementById("product-table");
+//   let newRow = table.insertRow(-1);
+//   let picCell = newRow.insertCell();
+//   let nameCell = newRow.insertCell();
+//   let priceCell = newRow.insertCell();
   
-  let picLink = document.createElement("img");
-  picLink.src = pic; 
-  picLink.classList.add("product-image");
+//   let picLink = document.createElement("img");
+//   picLink.src = pic; 
+//   picLink.classList.add("product-image");
 
-  let nameText = document.createElement("h2");
-  nameText.innerHTML = name;
+//   let nameText = document.createElement("h2");
+//   nameText.innerHTML = name;
 
-  let descriptionText = document.createTextNode(description);
-  let priceText = document.createElement("h3");
-  priceText.innerHTML = "$" + price;
+//   let descriptionText = document.createTextNode(description);
+//   let priceText = document.createElement("h3");
+//   priceText.innerHTML = "$" + price;
 
-  let addToCart = document.createElement("button");
-  addToCart.classList.add("addToCart");
-  addToCart.innerHTML = "Add to cart"
-  addToCart.name = name;
-  addToCart.value = price;
+//   let addToCart = document.createElement("button");
+//   addToCart.classList.add("addToCart");
+//   addToCart.innerHTML = "Add to cart"
+//   addToCart.name = name;
+//   addToCart.value = price;
 
-  picCell.appendChild(picLink);
-  nameCell.appendChild(nameText);
-  nameCell.appendChild(descriptionText);
-  nameCell.appendChild(priceText);
-  priceCell.appendChild(addToCart);
+//   picCell.appendChild(picLink);
+//   nameCell.appendChild(nameText);
+//   nameCell.appendChild(descriptionText);
+//   nameCell.appendChild(priceText);
+//   priceCell.appendChild(addToCart);
 
-  let cartButtons = document.querySelectorAll(".addToCart");
+//   let cartButtons = document.querySelectorAll(".addToCart");
 
-  cartButtons.forEach(item => {
-  item.addEventListener('click', cartHandler)
-  })
+//   cartButtons.forEach(item => {
+//   item.addEventListener('click', cartHandler)
+//   })
 
-})
-}
-getInventory();
+// })
+// }
+// getInventory();
 
 // start of shopping cart handler
-let order = [];
-const cartHandler = function() {
-  let addItem = {"name" : this.name, "price" : this.value};
-  let currentQuantity = parseInt(document.getElementById("order-quantity").innerHTML);
-  let updatedQuantity = currentQuantity + 1;
-  document.getElementById("order-quantity").innerHTML = updatedQuantity;
-  console.log(updatedQuantity);
-  order.push(addItem);
-  let stringOrder = JSON.stringify(order);
-  localStorage.setItem("order", stringOrder);
+// let order = [];
+// const cartHandler = function() {
+//   let addItem = {"name" : this.name, "price" : this.value};
+//   let currentQuantity = parseInt(document.getElementById("order-quantity").innerHTML);
+//   let updatedQuantity = currentQuantity + 1;
+//   document.getElementById("order-quantity").innerHTML = updatedQuantity;
+//   console.log(updatedQuantity);
+//   order.push(addItem);
+//   let stringOrder = JSON.stringify(order);
+//   localStorage.setItem("order", stringOrder);
 
-  let total = Number(localStorage.getItem("total"));
-  if (total) {
-    let itemValue = Number(this.value)
-    let newTotal = itemValue + total;
-    localStorage.setItem("total", newTotal);
-  } else {
-    localStorage.setItem("total", this.value);
-  }
+//   let total = Number(localStorage.getItem("total"));
+//   if (total) {
+//     let itemValue = Number(this.value)
+//     let newTotal = itemValue + total;
+//     localStorage.setItem("total", newTotal);
+//   } else {
+//     localStorage.setItem("total", this.value);
+//   }
+// }
+
+// Search for tho handler
+let searchButton = document.getElementById("search_button");
+
+const searchHandler = async function() {
+  let searchContent = document.getElementById("search_content");
+  let searchSummary = document.getElementById("search_summary");
+  let search = document.getElementById("search").value;
+  searchSummary.innerHTML = '';
+  searchContent.innerHTML = '';
+  let searchlUrl = "/api/cloud/postEmail" + "?search=" + search;
+  // let searchUrl = "https://faas-sgp1-18bc02ac.doserverless.co/api/v1/web/fn-7a547e63-4a3e-4a00-9798-b26f58c99213/algolia/getTho" + "?search=" + search;
+  const result = await axios.get(searchUrl);
+  // localStorage.setItem("search", search);
+  document.getElementById("search").value = '';
+
+  const message = "Có " + String(result.data.length) + " kết quả";
+  searchSummary.innerHTML = message;
+
+  result.data.forEach(e => {
+    let childTho = document.createElement('div');
+    childTho.innerHTML = marked.parse(e.content);
+    searchContent.appendChild(childTho);
+  })
 }
-
-// start of email subscription handler
-let subscribeButton = document.getElementById("subscribe");
-const subscribeHandler = async function() {
-  let email = document.getElementById("email").value
-  let emailUrl = "/api/cloud/postEmail" + "?email=" + email;
-  await axios.post(emailUrl);
-  localStorage.setItem("subscribe", email);
-  document.getElementById("email").value = '';
-
-  let emailForm = document.getElementById("email-form");
-  const message = "You have been successfully added to our email list."
-  const successMessage = document.createTextNode(message);
-  emailForm.appendChild(successMessage);
-}
-subscribeButton.addEventListener('click', subscribeHandler);
+searchButton.addEventListener('click', searchHandler);
 
 // start of shopping cart modal handler 
 
@@ -94,42 +104,42 @@ $('#myModal').on('shown.bs.modal', function () {
 const container = document.getElementById("testModal");
 const modal = new bootstrap.Modal(container);
 
-document.getElementById("btnShow").addEventListener("click", function () {
-  modal.show();
-  $('#modal-table tr:not(:first)').remove();
-  let orderData = JSON.parse(localStorage.getItem("order"));
-  let table = document.getElementById("modal-table");
+// document.getElementById("btnShow").addEventListener("click", function () {
+//   modal.show();
+//   $('#modal-table tr:not(:first)').remove();
+//   let orderData = JSON.parse(localStorage.getItem("order"));
+//   let table = document.getElementById("modal-table");
 
-  orderData.forEach(item => {
-  let name = item.name;
-  let price = item.price;
+//   orderData.forEach(item => {
+//   let name = item.name;
+//   let price = item.price;
 
-  let newRow = table.insertRow(-1);
-  let nameCell = newRow.insertCell();
-  let priceCell = newRow.insertCell();
+//   let newRow = table.insertRow(-1);
+//   let nameCell = newRow.insertCell();
+//   let priceCell = newRow.insertCell();
 
-  let nameText = document.createElement("p");
-  nameText.innerHTML = name;
+//   let nameText = document.createElement("p");
+//   nameText.innerHTML = name;
 
-  let priceText = document.createElement("p");
-  priceText.innerHTML = "$" + price;
+//   let priceText = document.createElement("p");
+//   priceText.innerHTML = "$" + price;
 
-  nameCell.appendChild(nameText);
-  priceCell.appendChild(priceText);
-  })
+//   nameCell.appendChild(nameText);
+//   priceCell.appendChild(priceText);
+//   })
   
-  let grandTotal = localStorage.getItem("total");
-  let newRow = table.insertRow(-1);
-  let totalCell = newRow.insertCell();
-  let grandTotalCell = newRow.insertCell();
+//   let grandTotal = localStorage.getItem("total");
+//   let newRow = table.insertRow(-1);
+//   let totalCell = newRow.insertCell();
+//   let grandTotalCell = newRow.insertCell();
 
-  let totalText = document.createElement("h3");
-  console.log(totalText); 
-  totalText.innerHTML = "Grand total: "
-  let grandTotalText = document.createElement("h3");
-  grandTotalText.innerHTML = "$" + grandTotal;
+//   let totalText = document.createElement("h3");
+//   console.log(totalText); 
+//   totalText.innerHTML = "Grand total: "
+//   let grandTotalText = document.createElement("h3");
+//   grandTotalText.innerHTML = "$" + grandTotal;
 
-  totalCell.appendChild(totalText);
-  grandTotalCell.appendChild(grandTotalText);
+//   totalCell.appendChild(totalText);
+//   grandTotalCell.appendChild(grandTotalText);
 
-});
+// });
